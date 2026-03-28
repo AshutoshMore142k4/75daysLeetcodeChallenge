6d081107feedd6adc8cc6907aea6cd6f3875404f
ash <148065446+AshutoshMore142k4@users.noreply.github.com>
2026-03-28 13:15:08 +0530

    Time: 35 ms (36.71%), Space: 14 MB (93.93%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,72 +1,17 @@
-//Mut1-[hash->sort+2ptr, map->static arrays]|O(nlogn)S(n)
+//Mut1-[for->while,rename]|O(n^2)S(1)
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& a, int goal) {
-        int n = (int)a.size();
-        static int val[10005], id[10005];
-        int i = 0;
-        while (i < n) {
-            val[i] = a[i];
-            id[i] = i;
-            ++i;
-        }
-
-        int gap = 1;
-        while (gap < n) gap <<= 1;
-
-        static int tmpVal[10005], tmpId[10005];
-        int len = 1;
-        while (len < n) {
-            int l = 0;
-            while (l < n) {
-                int m = l + len;
-                int r = m + len;
-                if (m > n) m = n;
-                if (r > n) r = n;
-
-                int p = l, q = m, k = l;
-                while (p < m && q < r) {
-                    if (val[p] <= val[q]) {
-                        tmpVal[k] = val[p];
-                        tmpId[k] = id[p];
-                        ++p;
-                    } else {
-                        tmpVal[k] = val[q];
-                        tmpId[k] = id[q];
-                        ++q;
-                    }
-                    ++k;
-                }
-                while (p < m) {
-                    tmpVal[k] = val[p];
-                    tmpId[k] = id[p];
-                    ++p;
-                    ++k;
-                }
-                while (q < r) {
-                    tmpVal[k] = val[q];
-                    tmpId[k] = id[q];
-                    ++q;
-                    ++k;
-                }
-                l = r;
+    vector<int> twoSum(vector<int>& buf, int aim) {
+        int m = (int)buf.size();
+        int p = 0;
+        while (p <= m - 1) {
+            int q = p + 1;
+            while (q <= m - 1) {
+                long long mix = (long long)buf[p] + buf[q];
+                if (mix == aim) return {p, q};
+                ++q;
             }
-
-            int t = 0;
-            while (t < n) {
-                val[t] = tmpVal[t];
-                id[t] = tmpId[t];
-                ++t;
-            }
-            len <<= 1;
-        }
-
-        int left = 0, right = n - 1;
-        while (left < right) {
-            long long cur = (long long)val[left] + val[right];
-            if (cur == goal) return {id[left], id[right]};
-            if (cur < goal) ++left;
-            else --right;
+            ++p;
         }
         return {};
     }
